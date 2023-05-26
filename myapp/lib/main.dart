1a03b04a39235b07690c567a3e933ec698e228d4
Jon <jonathancirillo@outlook.com>
2023-05-26 10:05:22 -0400

    revert all changes from today

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-<<<<<<< HEAD
-=======
-import 'package:flutter_dotenv/flutter_dotenv.dart' as dotenv;
->>>>>>> parent of b2dfd38 (Update main.dart)
 import 'config.dart';
 
 void main() async {
---
+++ myapp/web/config.dart
@@ -0,0 +1,2 @@
+const String firebaseClientId =
+    '878791846445-bv8orvfrfbovm37qh7ti3969l4lpk38l.apps.googleusercontent.com';
